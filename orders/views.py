ea9e39534086e9254ae8efc5e4f2096130ecd5ac
Burnnet <Burnnet00@mail.ru>
2023-04-17 12:14:44 +0300

    add img

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Product
 
 def home(reguest):
-    return render(reguest, 'orders/index.html')
+    bord = Product.objects.order_by('-date')
+    return render(reguest, 'orders/index.html', {'bord': bord})
 
 def about(reguest):
     return render(reguest, 'orders/about.html')
