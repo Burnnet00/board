946cfba4bf00e3f568af7364a2a5297c2c8f4ebd
Burnnet <Burnnet00@mail.ru>
2023-04-17 12:41:50 +0300

    debug about

--- books/urls.py
+++ books/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orders.urls')),
-    path('about', include('orders.urls')),
+    path('about/', include('orders.urls')),
 ]
 
 if settings.DEBUG:
