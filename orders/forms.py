d75fb0f6582fdc3ad04bea25c698bf88aeba31b0
Burnnet <Burnnet00@gmail.com>
2023-06-15 10:59:09 +0300

    add widget

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,13 @@
 from .models import Product
-from django.forms import ModelForm, TextInput, Textarea, FileInput, ImageField, DecimalField, ClearableFileInput
+from django.forms import ModelForm, TextInput, Textarea, FileInput, ImageField, NumberInput, ClearableFileInput
 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'image', 'description', 'price']
+        widgets = {'name': TextInput(attrs={"class": "form-control", "placeholder": "Введіть назву"}),
+                   'image': ClearableFileInput(attrs={"class": "form-control", "placeholder": "Світлина"}),
+                   'description': Textarea(attrs={"class": "form-control", "placeholder": "Опис"}),
+                   'price': NumberInput(attrs={"class": "form-control", "placeholder": "Ціна"})
+                   }
